2b785f32fa32e5a4ba0101eacfee37e8c7847488
vishalnikrad <155719351+vishalnikrad@users.noreply.github.com>
2024-04-27 15:45:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 
 app.get('/',function(req,res){
-    res.send('<h1>Hello Sir, can you please share that video of connection of SQL with render...</h1>')
+    res.send('<h1>Hello Sir, please share that video of connection with SQL using render...</h1>')
 })
 
 app.listen(3000,function(){
